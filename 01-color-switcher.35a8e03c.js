f110a1deba16086c2e2ef80b09bd425da47dca3b
mariavsl518 <134641398+mariavsl518@users.noreply.github.com>
2023-10-16 17:21:35 +0000

    Deploying to gh-pages from @ mariavsl518/goit-js-hw-09@60bd1144eaef1a3afd49547542113456b30a02b4 🚀

---
+++ 01-color-switcher.35a8e03c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),700),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.35a8e03c.js.map
